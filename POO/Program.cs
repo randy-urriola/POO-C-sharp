c52e9ec0b3452ee569ddf003b6fe85628969f611
randy-urriola <randy.urriolah@gmail.com>
2026-07-01 14:42:54 -0500

    Add project files.

---
+++ POO/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Objetos
+Botella botella1 = new Botella();
+Botella botella2 = new Botella();
+
+// Asignación de valores a las propiedades
+botella1.Color = "Black";
+botella1.Brand = "Bubba";
+botella2.Capacity = 32;
+botella2.Stickers = false;
+
+Console.WriteLine($"Botella 1: Color - {botella1.Color}, Marca - {botella1.Brand}.");
+Console.WriteLine($"Botella 2: Capacidad - {botella2.Capacity}oz, Tiene stickers? - {botella2.Stickers}.");
+
+// Clase
+class Botella
+{
+    // Propiedades
+    public string Color;
+    public string Brand;
+    public bool Stickers;
+    public double Height;
+    public double weight;
+    public double Capacity;
+}
